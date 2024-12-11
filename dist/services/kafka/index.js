2167ebf040f0a5eb92e77d9ad97d2c81d8087e51
ndtrong <nguyenductrong12a1@gmail.com>
2024-12-11 20:34:14 +0700

    Update: Ignore dist from repo

--- dist/services/kafka/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const kafkajs_1 = require("kafkajs");
-exports.default = new kafkajs_1.Kafka({
-    clientId: 'user-service',
-    brokers: [`${process.env.KAFKA_ADDRESS || 'localhost'}:${process.env.KAFKA_PORT || 9092}`],
-    logLevel: kafkajs_1.logLevel.ERROR
-});
-//# sourceMappingURL=index.js.map
